679ce30bcffd369b3c8bbfa0266cde42c6a4243d
Paulo Sergio <paulo-sergi0@hotmail.com>
2021-09-02 10:13:49 -0300

    simplemooc

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -154,6 +154,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static')
+#)
